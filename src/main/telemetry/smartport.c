31cb373973d2b024c247622ed2d6e53e3467f967
HaseebNain <haseebnain@u.boisestate.edu>
2017-01-18 09:45:49 -0700

    Smartport fix
    SmartPort hotfix
    - Disabled SERIAL_BIDIR and SERIAL_INVERTED through comment blocks.
    - CLI implementation is still bugged

--- src/main/telemetry/smartport.c
+++ src/main/telemetry/smartport.c
@@ -327,15 +327,17 @@ void configureSmartPortTelemetryPort(void)
     if (!portConfig) {
         return;
     }
-
+	
+	/*
     if (telemetryConfig->sportHalfDuplex) {
         portOptions = SERIAL_BIDIR;
     }
-    
+    */
+	/*
     if (telemetryConfig->telemetry_inversion) {
         portOptions |= SERIAL_INVERTED;
     }
-    
+    */
     smartPortSerialPort = openSerialPort(portConfig->identifier, FUNCTION_TELEMETRY_SMARTPORT, NULL, SMARTPORT_BAUD, SMARTPORT_UART_MODE, portOptions);
 
     if (!smartPortSerialPort) {
